51dca45c4eaa8d3dc187e22c4365bd3efa831a05
toppyy <topias.pyykkonen@helsinki.fi>
2022-09-29 20:51:16 +0300

    viikko2, teht. 3-6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,10 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many   :ratings
+
+    def average_rating
+        sum = ratings.map {|r| r.score}.reduce(:+)
+        return sum/ratings.count.to_f
+    end
+    
 end
