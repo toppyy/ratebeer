918c8dec0d977537ed6addf8917df04ad5b9a066
toppyy <topias.pyykkonen@helsinki.fi>
2022-10-02 17:12:07 +0300

    viikko2, teht. 15

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,11 +1,7 @@
 class Beer < ApplicationRecord
+    include RatingAverage
     belongs_to :brewery
     has_many :ratings, dependent: :destroy
-
-    def average_rating
-        sum = ratings.map {|r| r.score}.reduce(:+)
-        return sum/ratings.count.to_f
-    end
 
     def to_s
         return "#{name} (#{brewery.name})"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,5 @@
 class Brewery < ApplicationRecord
+    include RatingAverage 
     has_many :beers, dependent: :destroy
     has_many :ratings, through: :beers
 
