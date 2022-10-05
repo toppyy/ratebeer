1a579418884b0eb945def19aa6dbf7177e074059
toppyy <topias.pyykkonen@helsinki.fi>
2022-10-05 22:08:03 +0300

    viikko3, teht. 5 & 6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,8 +10,10 @@ class RatingsController < ApplicationController
   end
 
   def create
-    Rating.create params.require(:rating).permit(:score, :beer_id)
-    redirect_to ratings_path
+    rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+    rating.user = current_user
+    rating.save
+    redirect_to current_user
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  include RatingAverage
+  has_many :ratings
+
+  def to_s
+    "#{username}"
+  end
 end
