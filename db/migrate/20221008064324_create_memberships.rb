52f2f7afea54fef44865f2044cec0440c5667da8
toby <topias.pyykkonen@helsinki.fi>
2022-10-08 10:40:09 +0300

    fiksattu migraatiot

--- db/migrate/20221008064324_create_memberships.rb
+++ db/migrate/20221008064324_create_memberships.rb
@@ -2,7 +2,7 @@ class CreateMemberships < ActiveRecord::Migration[7.0]
   def change
     create_table :memberships do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :beerclub, null: false, foreign_key: true
+      t.references :beer_club, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20221008070437_fix_foreign_key_ref_membership.rb
+++
@@ -1,6 +0,0 @@
-class FixForeignKeyRefMembership < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :memberships, :beerclub, foreign_key: true
-    add_reference :memberships, :beer_club
-  end
-end
