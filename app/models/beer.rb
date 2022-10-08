ea2e52567e7e8f0107a773292916ade3a9d97877
toby <topias.pyykkonen@helsinki.fi>
2022-10-08 09:32:37 +0300

    n-n suhde olut-rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,7 @@ class Beer < ApplicationRecord
   include RatingAverage
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+  has_many :raters, -> { distinct }, through: :ratings, source: :user
 
   validates :name, presence: true
 
