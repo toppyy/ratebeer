c46048bfb2362687c86b5d657cbca1d1ee8435ab
toby <topias.pyykkonen@helsinki.fi>
2022-10-16 10:14:58 +0300

    viikko4, teht. 16

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -1,7 +1,7 @@
 class BeerClubsController < ApplicationController
   before_action :set_beer_club, only: %i[show edit update destroy]
   before_action :ensure_that_signed_in, except: [:index, :show]
-  
+
   # GET /beer_clubs or /beer_clubs.json
   def index
     @beer_clubs = BeerClub.all
