5426baf190261a8f7cc35c948ae77b51b4c9c62e
toby <topias.pyykkonen@helsinki.fi>
2022-10-21 17:27:03 +0300

    tyylifixit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,11 +12,10 @@ class PlacesController < ApplicationController
       redirect_to places_path, notice: "No places in #{params[:city]}"
     else
       places_from_api = [places_from_api] if places_from_api.is_a?(Hash)
-      @places = places_from_api.map do | location |
+      @places = places_from_api.map do |location|
         Place.new(location)
       end
       render :index, status: 418
     end
   end
-
 end
